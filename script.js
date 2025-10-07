4050993f46905df0404c36c54ae3d00c5582d446
Nikola <nikola042@onet.pl>
2025-10-07 09:19:57 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 (function () {
-  //TODO
+  const ex1Button = document.getElementById('ex1_button');
+  const ex1Content = document.getElementById('ex1_content');
+
+  ex1Button.addEventListener('click', function() {
+    let numbers = [];
+    for (let i = 0; i < 10; i++) {
+      numbers.push(i);
+    }
+    const resultString = numbers.join(', ');
+
+    ex1Content.textContent = resultString;
+  });
+
 })();
