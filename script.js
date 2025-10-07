4d433a7f8fef0f1be1671b0755200ea857e26381
Nikola <nikola042@onet.pl>
2025-10-07 09:35:20 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-(function () {
-  const ex1Button = document.getElementById('ex1_button');
-  const ex1Content = document.getElementById('ex1_content');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElement = document.getElementById("ex2_text");
+    const outputElement = document.getElementById('ex2_content');
 
-  ex1Button.addEventListener('click', function() {
-    let numbers = [];
-    for (let i = 0; i < 10; i++) {
-      numbers.push(i);
+    if (inputElement && outputElement) {
+        inputElement.addEventListener("input", validatePhoneNumber);
     }
-    const resultString = numbers.join(', ');
 
-    ex1Content.textContent = resultString;
-  });
+    function validatePhoneNumber() {
+        const phoneNumber = inputElement.value;
 
-})();
+        if (phoneNumber.length === 0) {
+            outputElement.textContent = "";
+            return;
+        }
+
+        // warunek 1
+        if (/[a-zA-Z]/.test(phoneNumber)) {
+            outputElement.textContent = "Numer nie może zawierać liter";
+            return;
+        }
+
+        //warunek 2
+        if (/[^0-9]/.test(phoneNumber)) {
+            outputElement.textContent = "Numer nie może zawierać znaków specjalnych";
+            return;
+        }
+
+        //warunek 3
+        if (phoneNumber.length !== 9) {
+            outputElement.textContent = "Długość numeru musi być równa 9";
+            return;
+        }
+
+        //warunek 4
+        outputElement.textContent = "Numer telefonu jest poprawny";
+    }
+});
